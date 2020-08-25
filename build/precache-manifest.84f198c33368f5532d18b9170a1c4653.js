56a49548f718b3a12374277fca6136166aa24205
mohammadreza <mrezavlb@gmail.com>
2020-08-25 14:50:37 +0430

    commited

---
+++ build/precache-manifest.84f198c33368f5532d18b9170a1c4653.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381ac193f315daa483e378b1c326f93a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "278e3e471f4c8a3545c0",
+    "url": "/static/css/main.19bed8b9.chunk.css"
+  },
+  {
+    "revision": "4c811755c4fb8cd197ad",
+    "url": "/static/js/2.4bd0603f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4bd0603f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "278e3e471f4c8a3545c0",
+    "url": "/static/js/main.e45c4596.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "8d0d4382d43d383900ae878a3f09a1f3",
+    "url": "/static/media/DigiShopLogo.8d0d4382.png"
+  },
+  {
+    "revision": "f44715fb07ed8357f2a62207a8c1735a",
+    "url": "/static/media/Sahel.f44715fb.woff"
+  },
+  {
+    "revision": "2ae1bf9040e3961c16bb9e75058b2abf",
+    "url": "/static/media/banner.2ae1bf90.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
